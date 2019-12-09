e8662d0d9ae7c711f6a5ee06e52780df4b1f395e
seiya0324 <p@seiyamurayama.com>
2019-12-09 00:00:08 -0800

    done tutorial-7

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ProjectDetails = props => {
+  const id = props.match.params.id;
+  return (
+    <div className="container section project-detail">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title - {id}</span>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur asperiores, architecto ducimus perspiciatis saepe illum, repellendus ab
+            deleniti iste natus aliquid. Libero ullam velit sequi, cumque laborum ipsa veritatis minima!
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>posted by the net ninja</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
